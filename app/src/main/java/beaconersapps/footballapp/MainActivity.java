adb2e4fdea4f4e2b969bd0bcd51c2292c316249a
valtercarita2017 <valtercarita.estagio2017@gmail.com>
2017-09-29 17:00:11 +0100

    Initial commit

---
+++ app/src/main/java/beaconersapps/footballapp/MainActivity.java
@@ -0,0 +1,13 @@
+package beaconersapps.footballapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
